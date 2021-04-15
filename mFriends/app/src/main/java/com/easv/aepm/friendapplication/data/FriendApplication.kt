4114da368845d1ce0f3393079c791469888ca996
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2021-04-15 09:29:34 +0200

    Changed name from ListViewRecyclerView to mFriends

--- mFriends/app/src/main/java/com/easv/aepm/friendapplication/data/FriendApplication.kt
+++ mFriends/app/src/main/java/com/easv/aepm/friendapplication/data/FriendApplication.kt
@@ -5,6 +5,7 @@ import com.easv.aepm.friendapplication.DAL.FriendRepository
 
 class FriendApplication : Application() {
 
+    //On app startup get information from repository
     override fun onCreate() {
         super.onCreate()
         FriendRepository.initialize(this)
